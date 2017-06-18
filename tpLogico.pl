e74732659c263d8500c805d77f368cbb8982d628
AguirreEze <aguirre.eze95@gmail.com>
2017-06-17 23:51:37 -0300

    Agrego Entrega 1
    Agrego Entrega 1

---
+++ tpLogico.pl
@@ -0,0 +1,43 @@
+%pareja(Persona, Persona)
+pareja(marsellus, mia).
+pareja(pumkin,honeyBunny).
+pareja(bernardo,bianca).
+pareja(bernardo,charo).
+ 
+%trabajaPara(Empleador, Empleado)
+trabajaPara(marsellus, vincent).
+trabajaPara(marsellus, jules).
+trabajaPara(marsellus, winston).
+trabajaPara(vincent, bernardo).
+trabajaPara(winston, bernardo).
+trabajaPara(bianca, george).
+trabajaPara(charo, george).
+
+
+saleCon(Quien,Cual):-
+	sonPareja(pareja(Quien,Cual)).
+
+sonPareja(pareja(Quien,Cual)):-
+	pareja(Cual,Quien).
+
+sonPareja(pareja(Quien,Cual)):-
+	pareja(Quien,Cual).
+
+esFiel(Persona):-
+	saleCon(Persona,_),
+	not(tieneDosParejas(Persona)).
+
+tieneDosParejas(Persona):-
+	 saleCon(Persona,Pareja1),
+	 saleCon(Persona,Pareja2),
+	 Pareja1 \= Pareja2.
+
+acataOrden(Empleador,Empleado):-
+	estanRelacionados(Empleador,Empleado).
+
+estanRelacionados(Empleador,Empleado):-
+	trabajaPara(Empleador, Empleado).
+
+estanRelacionados(Empleador1,Empleado):-
+	trabajaPara(Empleador1, Empleador2),
+	trabajaPara(Empleador2, Empleado).
